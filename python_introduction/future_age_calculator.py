8bcb0c96b1a09e8a7c8045e836014553c4d72260
Muluken <webruselitex41@gmail.com>
2024-09-12 23:00:30 +0300

    added str funciton in future age calculator file

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,4 +1,4 @@
 age = int(input("Enter your age"))
 future_age = age + 27
 
-print("In 2025, you will be " + future_age + " years old.")
+print("In 2025, you will be " + str(future_age) + " years old.")
