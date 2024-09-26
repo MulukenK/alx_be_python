f7ccff17ac7aba8b66345e77ebec5a3469e43917
Muluken <webruselitex41@gmail.com>
2024-09-27 00:56:52 +0300

    modified prompt messages

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -9,20 +9,28 @@ def convert_to_celsius(fahrenheit):
     celsius = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
     return celsius
 
-conversion = input("Pick what measure you want to convert to (Fahrenheit/Celsius): ").lower()
+temperature = int(input("Enter the temperature to convert:"))
 
-temperature = int(input("Is this temperature in Celsius or Fahrenheit? (C/F): "))
+conversion = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").lower()
 
-if conversion == "fahrenheit":
-    if 0 <= temperature <= 100:
-        print(f"{temperature}°C is {convert_to_fahrenheit(temperature)}°F")
-    else:
-        print("Please enter a temperature between 0 and 100°C.")
-        
-elif conversion == "celsius":
+
+
+if conversion == "f":
     if 32 <= temperature <= 212:
         print(f"{temperature}°F is {convert_to_celsius(temperature)}°C")
     else:
         print("Please enter a temperature between 32 and 212°F.")
+        
+elif conversion == "c":
+    if 0 <= temperature <= 100:
+        print(f"{temperature}°C is {convert_to_fahrenheit(temperature)}°F")
+    
+    else:
+        print("Please enter a temperature between 0 and 100°C.")
+
 else:
     print("Please enter a correct measurement (Fahrenheit or Celsius).")
+
+
+
+   
