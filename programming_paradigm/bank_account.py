9b84285e5c5384909f837a5ae6f9d71c43d8059f
Muluken <webruselitex41@gmail.com>
2024-10-06 15:45:53 +0300

    added bank account and main-0 files

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,31 @@
+
+
+
+class BankAccount:
+    def __init__(self, account_balance):
+        self.account_balance = account_balance
+        self.initial_balance = account_balance
+
+       
+    
+    def deposit(self, amount):
+        self.account_balance += amount
+        return self.account_balance
+    
+    def withdraw(self,amount):
+        if(self.account_balance >= amount):
+            self.account_balance -= amount
+            return True
+        else:
+            return False
+    
+    def display(self):
+        return f"Current Balance: ${self.account_balance}"
+
+
+
+
+
+
+
+        
