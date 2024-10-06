6bee9f0c5fd58ad31efc8e2aacbe26fb516b80f6
Muluken <webruselitex41@gmail.com>
2024-10-06 16:07:53 +0300

    added float on self.account balance

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -20,7 +20,7 @@ class BankAccount:
             return False
     
     def display_balance(self):
-       return print(f"Current Balance: ${self.account_balance}")
+       return print(f"Current Balance: ${float(self.account_balance)}")
 
 
 
