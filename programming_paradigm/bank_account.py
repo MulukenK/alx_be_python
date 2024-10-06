6dfc2dbeb5571bca38259aba2bb9cd08b72a6b03
Muluken <webruselitex41@gmail.com>
2024-10-06 15:54:48 +0300

    modified display balance method return

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -20,7 +20,7 @@ class BankAccount:
             return False
     
     def display_balance(self):
-        return f"Current Balance: ${self.account_balance}"
+        print(f"Current Balance: ${self.account_balance}")
 
 
 
