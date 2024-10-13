79ffdcb3025a5ee25986aaa05321ddc1b790b6d7
Muluken <webruselitex41@gmail.com>
2024-10-13 06:42:56 +0300

    added str funtion to all

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,15 +3,25 @@ class Book:
         self.title = title
         self.author = author
     
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+    
 class EBook(Book):
     def __init__(self, file_size):
         self.file_size = file_size
         super().__init__()
+    
+    def __str__(self):
+        return f"{self.title} by {self.author}, File Size: {self.file_size}"
+
 
 class PrintBook(Book):
     def __init__(self, page_count):
         self.page_count = page_count
         super().__init__()
+    
+    def __str__(self):
+        return f"{self.title} by {self.author}, Page Count: {self.page_count}"
 
 class Library():
     def __init__(self, book):
